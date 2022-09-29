bc3e5767dd72aff0b09fda7b25eaa15da2190957
Quessada <aquessada@gmail.com>
2022-09-29 14:58:49 -0300

    Added Parameters

--- Source/PluginProcessor.h
+++ Source/PluginProcessor.h
@@ -24,8 +24,8 @@
 
 namespace Params
 {
-    enum Names
-    {
+enum Names
+{
         Low_Crossover_Freq,
         Low_Mid_Crossover_Freq,
         Mid_High_Crossover_Freq,
@@ -50,7 +50,44 @@ namespace Params
         Ratio_Low_Mid_Band,
         Ratio_Mid_High_Band,
         Ratio_High_Band,
+
+        
+        Bypassed_Low_Band,
+        Bypassed_Low_Mid_Band,
+        Bypassed_Mid_High_Band,
+        Bypassed_High_Band,
     };
+inline const std::map<Names, juce::String>& GetParams()
+{
+    static std::map<Names, juce::String> params = {
+
+    {Low_Crossover_Freq,"Low Crossover Freq"},
+    {Low_Mid_Crossover_Freq, "Low Mid Crossover Freq"},
+    {Mid_High_Crossover_Freq, "Mid High Crossover Freq"},
+    {High_Crossover_Freq, "High Crossover Freq"},
+    {Threshold_Low_Band, "Threshold Low Band"},
+    {Threshold_Low_Mid_Band, "Threshold Low Mid Band"},
+    {Threshold_Mid_High_Band, "Threshold Mid High Band"},
+    {Threshold_High_Band, "Threshold High Band"},
+    {Attack_Low_Band, "Attack Low Band"},
+    {Attack_Low_Mid_Band, "Attack Low Mid Band"},
+    {Attack_Mid_High_Band, "Attack Mid High Band"},
+    {Attack_High_Band, "Attack High Band"},
+    {Release_Low_Band, "Release Low Band"},
+    {Release_Low_Mid_Band, "Release Low Mid Band"},
+    {Release_Mid_High_Band, "Release Mid High Band"},
+    {Release_High_Band, "Release High Band"},
+    {Ratio_Low_Band, "Ratio Low Band"},
+    {Ratio_Low_Mid_Band, "Ratio Low Mid Band"},
+    {Ratio_Mid_High_Band, "Ratio Mid High Band"},
+    {Ratio_High_Band, "Ratio High Band"},
+    {Bypassed_Low_Band, "Bypassed Low Band"},
+    {Bypassed_Low_Mid_Band, "Bypassed Low Mid Band"},
+    {Bypassed_Mid_High_Band, "Bypassed Mid High Band"},
+    {Bypassed_High_Band, "Bypassed_High_Band"},
+    };
+    return params;
+}
 }
 
 
