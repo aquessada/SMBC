796593d7f6e9de61cb49ad46d7d6909028b770be
Quessada <aquessada@gmail.com>
2022-10-12 11:38:05 -0300

    Fixed neginf line beeing visible in Spectrum Analyzer
    Refactored how FFT paths are drawn

--- Source/GUI/SpectrumAnalyzer.cpp
+++ Source/GUI/SpectrumAnalyzer.cpp
@@ -35,7 +35,30 @@ SpectrumAnalyzer::~SpectrumAnalyzer()
         param->removeListener(this);
     }
 }
+void SpectrumAnalyzer::drawFFTAnalysis(juce::Graphics& g, juce::Rectangle<int> bounds)
+{
+    using namespace juce;
+    auto responseArea = getAnalysisArea(bounds);
 
+    Graphics::ScopedSaveState sss(g);
+    g.reduceClipRegion(responseArea);
+
+    auto leftChannelFFTPath = leftPathProducer.getPath();
+    leftChannelFFTPath.applyTransform(AffineTransform().translation(responseArea.getX(), 0
+        //responseArea.getY())
+    ));
+
+    g.setColour(Colour(97u, 18u, 167u)); //purple-
+    g.strokePath(leftChannelFFTPath, PathStrokeType(1.f));
+
+    auto rightChannelFFTPath = rightPathProducer.getPath();
+    rightChannelFFTPath.applyTransform(AffineTransform().translation(responseArea.getX(), 0
+        //responseArea.getY())
+    ));
+
+    g.setColour(Colour(215u, 201u, 134u));
+    g.strokePath(rightChannelFFTPath, PathStrokeType(1.f));
+}
 void SpectrumAnalyzer::paint(juce::Graphics& g)
 {
     using namespace juce;
@@ -46,25 +69,12 @@ void SpectrumAnalyzer::paint(juce::Graphics& g)
 
     drawBackgroundGrid(g, bounds);
 
-    auto responseArea = getAnalysisArea(bounds);
+    
 
     if (shouldShowFFTAnalysis)
     {
-        auto leftChannelFFTPath = leftPathProducer.getPath();
-        leftChannelFFTPath.applyTransform(AffineTransform().translation(responseArea.getX(), 0
-                                              //responseArea.getY())
-                                              ));
-
-        g.setColour(Colour(97u, 18u, 167u)); //purple-
-        g.strokePath(leftChannelFFTPath, PathStrokeType(1.f));
-
-        auto rightChannelFFTPath = rightPathProducer.getPath();
-        rightChannelFFTPath.applyTransform(AffineTransform().translation(responseArea.getX(), 0
-                                                //responseArea.getY())
-                                            ));
-
-        g.setColour(Colour(215u, 201u, 134u));
-        g.strokePath(rightChannelFFTPath, PathStrokeType(1.f));
+        drawFFTAnalysis(g, bounds);
+        
     }
 
 
--- Source/GUI/SpectrumAnalyzer.h
+++ Source/GUI/SpectrumAnalyzer.h
@@ -61,4 +61,6 @@ private:
     juce::Rectangle<int> getAnalysisArea(juce::Rectangle<int> bounds);
 
     PathProducer leftPathProducer, rightPathProducer;
+
+    void drawFFTAnalysis(juce::Graphics& g, juce::Rectangle<int> bounds);
 };
