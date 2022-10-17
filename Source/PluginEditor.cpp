6fedfadcf7800601c0b6e384fadb4bcf8a5ca163
Quessada <aquessada@gmail.com>
2022-10-17 17:47:46 -0300

    Implemented Global Controls

--- Source/PluginEditor.cpp
+++ Source/PluginEditor.cpp
@@ -6,10 +6,25 @@
 
 #include "PluginProcessor.h"
 #include "PluginEditor.h"
+#include "DSP/Params.h"
 
 //==============================================================================
 
+ControlBar::ControlBar()
+{
+    analyzerButton.setToggleState(true, juce::NotificationType::dontSendNotification);
+    addAndMakeVisible(analyzerButton);
+    addAndMakeVisible(globalBypassButton);
+}
 
+void ControlBar::resized()
+{
+    auto bounds = getLocalBounds();
+
+    analyzerButton.setBounds(bounds.removeFromLeft(50).withTrimmedTop(4).withTrimmedBottom(4));
+
+    globalBypassButton.setBounds(bounds.removeFromRight(60).withTrimmedTop(2).withTrimmedBottom(2));
+}
 
 //==============================================================================
 SimpleMBCompAudioProcessorEditor::SimpleMBCompAudioProcessorEditor(SimpleMBCompAudioProcessor& p)
@@ -18,7 +33,19 @@ SimpleMBCompAudioProcessorEditor::SimpleMBCompAudioProcessorEditor(SimpleMBCompA
     // Make sure that before the constructor has finished, you've set the
     // editor's size to whatever you need it to be.
     setLookAndFeel(&lnf);
-    //    addAndMakeVisible(controlBar);
+
+    controlBar.globalBypassButton.onClick = [this]()
+    {
+        toggleGlobalBypassState();
+    };
+
+    controlBar.analyzerButton.onClick = [this]()
+    {
+        auto shouldBeOn = controlBar.analyzerButton.getToggleState();
+        analyzer.toggleAnalysisEnablement(shouldBeOn);
+    };
+
+    addAndMakeVisible(controlBar);
     addAndMakeVisible(analyzer);
 
     addAndMakeVisible(globalControls);
@@ -64,7 +91,7 @@ void SimpleMBCompAudioProcessorEditor::resized()
 }
 
 void SimpleMBCompAudioProcessorEditor::timerCallback()
-{ 
+{
     std::vector<float> values
     {
         audioProcessor.LowBandComp.getRMSInputLevelDb(),
@@ -77,4 +104,68 @@ void SimpleMBCompAudioProcessorEditor::timerCallback()
         audioProcessor.HighBandComp.getRMSOutputLevelDb()
     };
     analyzer.update(values);
-};
+
+    updateGlobalBypassButton();
+}
+
+void SimpleMBCompAudioProcessorEditor::updateGlobalBypassButton()
+{
+    auto params = getBypassParams();
+
+    bool allBandsAreBypassed = std::all_of(params.begin(), params.end(),
+        [](const auto& param) {return param->get(); });
+
+    controlBar.globalBypassButton.setToggleState(allBandsAreBypassed, juce::NotificationType::dontSendNotification);
+}
+
+    void SimpleMBCompAudioProcessorEditor::toggleGlobalBypassState()
+    {
+        auto shouldEnableEverything =  ! controlBar.globalBypassButton.getToggleState();
+
+        auto params = getBypassParams();
+
+        auto bypassParamHelper = [](auto* param, bool shouldBeBypassed)
+        {
+            param->beginChangeGesture();
+            param->setValueNotifyingHost(shouldBeBypassed ? 1.f : 0.f);
+            param->endChangeGesture();
+        };
+
+        for (auto* param : params)
+        {
+            bypassParamHelper(param, !shouldEnableEverything);
+        }
+
+        bandControls.toggleAllBands(!shouldEnableEverything);
+    }
+
+    std::array<juce::AudioParameterBool*, 4> SimpleMBCompAudioProcessorEditor::getBypassParams()
+    {
+        using namespace Params;
+        using namespace juce;
+        const auto& params = Params::GetParams();
+        auto& apvts = audioProcessor.apvts;
+
+        auto boolHelper = [&apvts, &params](const auto& paramName)
+        {
+            auto param = dynamic_cast<juce::AudioParameterBool*>(apvts.getParameter(params.at(paramName)));
+            jassert(param != nullptr);
+
+            return param;
+        };
+
+        auto lowBypassParam = boolHelper(Names::Bypassed_Low_Band);
+        auto lowMidBypassParam = boolHelper(Names::Bypassed_Low_Mid_Band);
+        auto midHighBypassParam = boolHelper(Names::Bypassed_Mid_High_Band);
+        auto highBypassParam = boolHelper(Names::Bypassed_High_Band);
+
+        return
+        {
+            lowBypassParam,
+            lowMidBypassParam,
+            midHighBypassParam,
+            highBypassParam
+        };
+    }
+
+
